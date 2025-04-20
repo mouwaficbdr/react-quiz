4d091b63912109ba72ad78ec0ae8ac870f97976b
Mouwafic Badarou <badaroumouwafic@gmail.com>
2025-04-20 16:07:20 +0100

    feat: ajouter les composants DateCounter, Error, Header, Loader, MainComponent, Question et StartScreen

---
+++ src/components/Question.jsx
@@ -0,0 +1,12 @@
+import Options from "./Options"
+
+const Question = ({ question }) => {
+  return (
+    <div>
+      <h4>{question.question}</h4>
+      <Options question={question}/>
+    </div>
+  );
+};
+
+export default Question;
