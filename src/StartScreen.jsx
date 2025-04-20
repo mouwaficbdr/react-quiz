07a04ee743a7a9344043290aab88a67c3274804c
Mouwafic Badarou <badaroumouwafic@gmail.com>
2025-04-20 14:51:23 +0100

    feat: ajouter le composant StartScreen pour l'écran de démarrage du quiz

---
+++ src/StartScreen.jsx
@@ -0,0 +1,11 @@
+const StartScreen = ({ numQuestions }) => {
+  return (
+    <div>
+      <h2>Welcom to The React Quiz!</h2>
+      <h3>{numQuestions} questions to test your React mastery</h3>
+      <button className="btn btn-ui">Let's start</button>
+    </div>
+  );
+};
+
+export default StartScreen;
