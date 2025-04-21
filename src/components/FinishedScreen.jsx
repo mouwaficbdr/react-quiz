26e13c031cb4896387acd91072414e02577a931c
Mouwafic Badarou <badaroumouwafic@gmail.com>
2025-04-21 16:20:56 +0100

    feat: ajouter la gestion du redémarrage dans le composant FinishedScreen

--- src/components/FinishedScreen.jsx
+++ src/components/FinishedScreen.jsx
@@ -1,6 +1,5 @@
-const FinishedScreen = ({points, maxPossiblePoints, highscore}) => {
-
-  const percentage = (points/maxPossiblePoints) * 100
+const FinishedScreen = ({ points, maxPossiblePoints, highscore, dispatch }) => {
+  const percentage = (points / maxPossiblePoints) * 100;
 
   let emoji;
   if (percentage === 100) emoji = '🥇';
@@ -12,10 +11,18 @@ const FinishedScreen = ({points, maxPossiblePoints, highscore}) => {
   return (
     <>
       <p className="result">
-        <span>{emoji}</span>You scored <strong>{points}</strong> out of {maxPossiblePoints} (
-        {Math.ceil(percentage)}%)
+        <span>{emoji}</span>You scored <strong>{points}</strong> out of{' '}
+        {maxPossiblePoints} ({Math.ceil(percentage)}%)
       </p>
       <p className="highscore">(Highscore: {highscore} points)</p>
+      <button
+        className="btn btn-ui"
+        onClick={() => {
+          dispatch({ type: 'restart' });
+        }}
+      >
+        Restart
+      </button>
     </>
   );
 };
